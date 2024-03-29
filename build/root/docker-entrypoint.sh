040db3a35924ce28cdd4dffb66a139ab32003bfb
Carl Park <dkdkajej@gmail.com>
2024-03-29 16:10:37 +0900

    format

--- build/root/docker-entrypoint.sh
+++ build/root/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # ln_if_file_exists src dest
-function ln_if_file_exists () {
-    if [ -e "$1" ]; then
-        ln -sf "$1" "$2"
-    fi
+function ln_if_file_exists() {
+  if [ -e "$1" ]; then
+    ln -sf "$1" "$2"
+  fi
 }
 
 # Symlink logfiles
@@ -18,11 +18,11 @@ ln_if_file_exists "/proc/1/fd/1" "/var/log/auth.log"
 ln_if_file_exists "/proc/1/fd/1" "/var/log/syslog"
 
 # Setup roundcube des_key
-RC_DES_KEY=`cat /dev/urandom | head -n 256 | sha256sum | awk '{print $1}'`;
+RC_DES_KEY=$(cat /dev/urandom | head -n 256 | sha256sum | awk '{print $1}')
 sed -i "s/###DES_KEY###/$RC_DES_KEY/" /etc/roundcube/config.inc.php
 # Configure proxy path for use behind a reverse proxy
-if [ -z ${MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH} ];then
-  sed -i "s|###MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH###|null|" /etc/roundcube/config.inc.php  
+if [ -z ${MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH} ]; then
+  sed -i "s|###MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH###|null|" /etc/roundcube/config.inc.php
 else
   sed -i "s|###MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH###|'$MAILTRAP_ROUNDCUBE_CONFIG_REQUEST_PATH'|" /etc/roundcube/config.inc.php
 fi
@@ -31,8 +31,8 @@ fi
 make-ssl-cert generate-default-snakeoil --force-overwrite
 
 # generate dovecote userdb
-echo -n "$MAILTRAP_USER:" > /etc/dovecot/passwd.db
-doveadm pw -s SHA512-CRYPT >> /etc/dovecot/passwd.db <<EOF
+echo -n "$MAILTRAP_USER:" >/etc/dovecot/passwd.db
+doveadm pw -s SHA512-CRYPT >>/etc/dovecot/passwd.db <<EOF
 ${MAILTRAP_PASSWORD}
 ${MAILTRAP_PASSWORD}
 EOF
@@ -44,7 +44,7 @@ sed -i "s/###MAILTRAP_MAX_RECIPIENT_LIMIT###/$MAILTRAP_MAX_RECIPIENT_LIMIT/" /et
 
 # Configure services and transports in master.cf
 sed -i "s/#submission inet n       -       y       -       -       smtpd/submission inet n       -       y       -       -       smtpd/" /etc/postfix/master.cf
-echo -e "dovecot unix - n n - - pipe flags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/deliver -f \${sender} -d ${MAILTRAP_USER}" >> /etc/postfix/master.cf
+echo -e "dovecot unix - n n - - pipe flags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/deliver -f \${sender} -d ${MAILTRAP_USER}" >>/etc/postfix/master.cf
 
 # Configure api php-script
 sed -i "s/###MAILTRAP_USER###/$MAILTRAP_USER/" /usr/share/roundcube/api.php
